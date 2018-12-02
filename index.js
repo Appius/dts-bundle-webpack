2d42ac0fc2f5c0460de26e1f682fbc29ae659581
Vladyslav Ushakov <appius@outlook.com>
2018-12-02 13:32:51 +0200

    released version 1.0.1

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 'use strict';
 var DtsBundlePlugin = (function () {
-    const dts = require('dts-bundle');
+  const dts = require('dts-bundle');
 
-    function DtsBundlePlugin(options){
-        if (options === void 0) { options = {}; }
-        this.options = options;
+  function DtsBundlePlugin(options){
+    if (options === void 0) { options = {}; }
+    this.options = options;
+  }
+
+  function _bundle(options) {
+    return () => dts.bundle(options);
+  }
+
+  DtsBundlePlugin.prototype.apply = function (compiler) {
+    const bundle = () => _bundle(this.options);
+
+    if (!!compiler.hooks) {
+      compiler.hooks.done.tap('DtsBundlePlugin', bundle());
+    } else {
+      compiler.plugin('done', bundle());
     }
+  };
 
-    DtsBundlePlugin.prototype.apply = function (compiler) {
-        const _this = this;
-
-        compiler.hooks.done.tap({ name: 'DtsBundlePlugin' }, function() {
-            dts.bundle(_this.options);
-        });
-    };
-
-    return DtsBundlePlugin;
+  return DtsBundlePlugin;
 })();
 
 module.exports = DtsBundlePlugin;
